7ed153d9da2642cc14c2e3eb87df276e386d6f54
b2blosangeles <b2blosangeles@gmail.com>
2020-03-02 22:28:01 -0800

    Update cron.sh

--- shell/cron.sh
+++ shell/cron.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-FILES=/var/qalet/cron
-for f in $FILES
+folder=/var/qalet/cron
+for f in "$folder"/*
 do
   echo "Processing $f file..."
   # take action on each file. $f store current file name
-  # sh $f
-  # rm -fr $f
+  sh $f
 done
