d61c778b7d4e5484bc39e84e593f1e841f3b8c94
b2blosangeles <b2blosangeles@gmail.com>
2020-03-11 08:57:10 -0700

    Update secCron.sh

--- secCron.sh
+++ secCron.sh
@@ -6,3 +6,12 @@ then
 else
    echo "a is not 36"
 fi
+#!/bin/bash
+mkdir -p /tmp/task
+folder=/tmp/task
+for f in "$folder"/*
+do
+  echo "Processing $f task..."
+  # take action on each file. $f store current file name
+  sh $f
+done
