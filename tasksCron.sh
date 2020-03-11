98ae3c0cf38e1693c1ab960cbbb37297a3aacf9f
b2blosangeles <b2blosangeles@gmail.com>
2020-03-11 09:25:44 -0700

    Update tasksCron.sh

--- tasksCron.sh
+++ tasksCron.sh
@@ -2,9 +2,13 @@
 mkdir -p /tmp/tasks
 folder=/tmp/tasks
 for f in "$folder"/*
+fn = "$folder/$f"
 do
-  echo "Processing $f task..."
-  # take action on each file. $f store current file name
-  sh $f
-  rm -f $f
+  if [[ -f "$fn" ]]; then
+    echo "Processing $fn task..."
+    # take action on each file. $f store current file name
+    sh $fn
+    # rm -f $f     
+  fi
+
 done
