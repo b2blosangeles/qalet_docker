5f910a879887c4ef75e702f346d3a79f22ae61b4
b2blosangeles <b2blosangeles@gmail.com>
2020-03-17 15:40:57 -0700

    Update cronScript.sh

--- qalet_admin/localCron/cronScript.sh
+++ qalet_admin/localCron/cronScript.sh
@@ -1,15 +1,17 @@
 qaletFolderMaster="/var/qalet/master"
 qaletFolderAdmin="/var/qalet/admin"
-qaletFolderAdmin="/var/qalet/proxy"
+qaletFolderProxy="/var/qalet/proxy"
 qaletFolderSites="/var/qalet/sites"
 qaletFolderSetup="/var/qalet/master/setup"
 qaletFolderTasks="/var/qalet/tasks"
 
-mkdir -p $qaletFolderMaster && mkdir -p $qaletFolderSites &&\
+mkdir -p $qaletFolderMaster && mkdir -p $qaletFolderSites
 mkdir -p $qaletFolderTasks && mkdir -p $qaletFolderAdmin && mkdir -p $qaletFolderSetup
 
 mkdir -p "$qaletFolderSetup/log"
 cd $qaletFolderSetup && git pull
 
-cp -rf "$qaletFolderSetup/qalet_admin/." "$qaletFolderAdmin/" && rm -fr "$qaletFolderAdmin/Dockerfile" \
+cp -rf "$qaletFolderSetup/qalet_admin/." "$qaletFolderAdmin/" && rm -fr "$qaletFolderAdmin/Dockerfile"
+cp -rf "$qaletFolderSetup/Docker-Httpd-Reverseproxy/." "$qaletFolderProxy/" && rm -fr "$qaletFolderProxy/Dockerfile"
+
 node /var/qalet/localCron/codeUpdate.js >> /var/qalet/log/codeUpdate.js.log
