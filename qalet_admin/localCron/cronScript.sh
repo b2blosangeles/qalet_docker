8f4922964c174e2c44bb247ffbc1b1a3f481a260
b2blosangeles <b2blosangeles@gmail.com>
2020-03-17 15:31:46 -0700

    Update cronScript.sh

--- qalet_admin/localCron/cronScript.sh
+++ qalet_admin/localCron/cronScript.sh
@@ -1,10 +1,12 @@
 qaletFolderMaster="/var/qalet/master"
 qaletFolderAdmin="/var/qalet/admin"
+qaletFolderAdmin="/var/qalet/proxy"
 qaletFolderSites="/var/qalet/sites"
 qaletFolderSetup="/var/qalet/master/setup"
 qaletFolderTasks="/var/qalet/tasks"
 
-mkdir -p $qaletFolderMaster && mkdir -p $qaletFolderSites && mkdir -p $qaletFolderTasks && mkdir -p $qaletFolderAdmin && mkdir -p $qaletFolderSetup
+mkdir -p $qaletFolderMaster && mkdir -p $qaletFolderSites &&\
+mkdir -p $qaletFolderTasks && mkdir -p $qaletFolderAdmin && mkdir -p $qaletFolderSetup
 
 mkdir -p "$qaletFolderSetup/log"
 cd $qaletFolderSetup && git pull
