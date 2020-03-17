f66a1df4f3a8fc0796a6ae67eacf9fd81f775d91
b2blosangeles <b2blosangeles@gmail.com>
2020-03-17 09:16:13 -0700

    Update cronScript.sh

--- qalet_admin/localCron/cronScript.sh
+++ qalet_admin/localCron/cronScript.sh
@@ -1,1 +1,2 @@
-node /var/qalet/localCron/codeUpdate.js >> /var/qalet_tasks/log/codeUpdate.js.log
+mkdir -p /var/qalet/log
+node /var/qalet/localCron/codeUpdate.js >> /var/qalet/log/codeUpdate.js.log
