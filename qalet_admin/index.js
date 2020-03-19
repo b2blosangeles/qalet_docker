4a0ef176613de022d2b5192c5306e5a494ba01a8
b2blosangeles <b2blosangeles@gmail.com>
2020-03-18 19:22:16 -0700

    Update index.js

--- qalet_admin/index.js
+++ qalet_admin/index.js
@@ -1,3 +1,4 @@
+/*
 var http = require('http');
 var fs = require('fs');
 http.createServer(function (req, res) {
@@ -7,3 +8,18 @@ http.createServer(function (req, res) {
     res.end();
   });
 }).listen(80);
+*/
+var express = require('express');
+var app = express();
+var ECT = require('ect');
+var ectRenderer = ECT({ watch: true, root: __dirname + '/views', ext : '.ect' });
+
+// app.set('view engine', 'ect');
+app.engine('ect', ectRenderer.render);
+
+app.get('/', function (req, res){
+    res.render('index.ect');
+});
+
+app.listen(80);
+console.log('Listening on port 80');
