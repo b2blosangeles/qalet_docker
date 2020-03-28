843431f44615e5936225f365d6cb11a3c880d68c
b2blosangeles <b2blosangeles@gmail.com>
2020-03-28 14:38:57 -0700

    Update siteUpdate.js

--- qalet_admin/localCron/siteUpdate.js
+++ qalet_admin/localCron/siteUpdate.js
@@ -14,14 +14,34 @@ fs.readdir(env.sitesPath, function (err, files) {
     var CP = new pkg.crowdProcess(),_f = {}; 
     files.forEach(function (file) {
         _f[file] = function(cbk) {
-              var cmd = "cd " + env.sitesPath + "/" + file +  " && git pull"
-              pkg.exec(cmd, 
-                  {maxBuffer: 1024 * 2048},
-                  function(error, stdout, stderr) {
-                     let status = stdout.replace(/\r?\n|\r/g, '');
-                     if (status == 'updated') CP.exit = 1;
-                     cbk(status);
-             });
+              var CP0 = new pkg.crowdProcess(),_f0 = {};
+              _f0['checkUpdate'] = function(cbk0) {
+                    var cmd0 = "cd " + env.sitesPath + "/" + file + " && " +
+                    " if [ $(git rev-parse HEAD) = $(git ls-remote $(git rev-parse --abbrev-ref) | head -n1 | cut -f1) ]; then echo 'updated' ; else echo 'changed' ; fi"
+                    pkg.exec(cmd0, 
+                         {maxBuffer: 1024 * 2048},
+                         function(error, stdout, stderr) {
+                            let status = stdout.replace(/\r?\n|\r/g, '');
+                            if (status == 'updated') CP0.exit = 1;
+                            cb0k(status);
+                    });
+                  }
+                  _f0['gitPull'] = function(cb0k) {
+                     var cmd0 = "cd " + env.sitesPath + "/" + file + " && git pull";
+                     pkg.exec(cmd0, 
+                         {maxBuffer: 1024 * 2048},
+                         function(error, stdout, stderr) {
+                           let status = stdout.replace(/\r?\n|\r/g, '');
+                           cbk0(status);
+                     });
+                  }
+                  CP0.serial(
+                    _f0,
+                    function(data) {
+                        cbk(data);
+                      },
+                      20000
+                  );
         }
     });
     CP.serial(
@@ -35,42 +55,3 @@ fs.readdir(env.sitesPath, function (err, files) {
 
 console.log(__dirname);
 console.log('Run admin at : ' + new Date());
-
-/*
-console.log('End admin at : ' + new Date());
-
-var CP = new pkg.crowdProcess(),_f = {}; 
-    _f['checkUpdate'] = function(cbk) {
-      var cmd = "cd " + env.rootPath + "/master/setup && " +
-      " if [ $(git rev-parse HEAD) = $(git ls-remote $(git rev-parse --abbrev-ref) | head -n1 | cut -f1) ]; then echo 'updated' ; else echo 'changed' ; fi"
-      pkg.exec(cmd, 
-           {maxBuffer: 1024 * 2048},
-           function(error, stdout, stderr) {
-              let status = stdout.replace(/\r?\n|\r/g, '');
-              if (status == 'updated') CP.exit = 1;
-              cbk(status);
-      });
-    }
-
-    _f['gitPull'] = function(cbk) {
-           var qaletFolderSetup= env.rootPath + "/master/setup";
-           var qaletFolderAdmin= env.rootPath + "/admin";
-           var qaletFolderProxy= env.rootPath + "/proxy";
-           var cmd = "cd " + qaletFolderSetup + " && git pull && " + 
-               "cp -rf " + qaletFolderSetup + "/qalet_admin/. " + qaletFolderAdmin + "/ && rm -fr "+ qaletFolderAdmin + "/Dockerfile && " +
-               "cp -rf " + qaletFolderSetup + "/docker-httpd-reverseproxy/. " + qaletFolderProxy +"/ && rm -fr "+ qaletFolderProxy + "/Dockerfile";
-      pkg.exec(cmd, 
-           {maxBuffer: 1024 * 2048},
-           function(error, stdout, stderr) {
-             let status = stdout.replace(/\r?\n|\r/g, '');
-             cbk(status);
-      });
-    }
-    CP.serial(
-      _f,
-      function(data) {
-          console.log(data);
-        },
-        55000
-    );
-  */
