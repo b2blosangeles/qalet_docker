d4e45b6e66ec3006d5576ac3cdc26cb729ad62a6
b2blosangeles <b2blosangeles@gmail.com>
2020-03-30 13:19:35 -0700

    Update appRouter.js

--- qalet_admin/modules/appRouter.js
+++ qalet_admin/modules/appRouter.js
@@ -132,11 +132,11 @@
 			var TPA = {
 				"/"	: {
 						tpl : "html/index.ect",
-						data : {module_code: 'home', module : "Home"}
+						data : {module : "Home"}
 				},
 				"/about": {
 						tpl : "html/about.ect",
-						data : {module_code: 'about', module : "About"}
+						data : {module : "About"}
 					}
 			}
 			
