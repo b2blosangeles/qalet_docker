8753e3bb2d6a94263ff3f51822b5502346038ae4
b2blosangeles <b2blosangeles@gmail.com>
2020-03-31 18:03:47 -0700

    Update appRouter.js

--- qalet_admin/modules/appRouter.js
+++ qalet_admin/modules/appRouter.js
@@ -17,7 +17,7 @@
 			// var CP = new pkg.crowdProcess(),_f = {}; 
 			var str = "echo 'niu-" + new Date() + "' >> /tmp/niub.log\n";
 			fs.writeFile('/var/qalet_tasks/niu.sh', str, function(err){
-			    res.send(err.message + '=>' + str);
+			    res.send('=>' + str);
 			});
 		}
 		this.checkCodeUpdate = function() {
