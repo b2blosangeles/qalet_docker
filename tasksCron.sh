1be795ad8792ab1e52d5dc6f30dc36110791cf54
b2blosangeles <b2blosangeles@gmail.com>
2020-03-31 19:45:21 -0700

    Update tasksCron.sh

--- tasksCron.sh
+++ tasksCron.sh
@@ -5,8 +5,8 @@ for f in "$folder"/*; do
   if [ -f "$f" ]; then
     echo "Processing $f task..."
     fn=/tmp/SH$(($(date +%s%N)/1000000)).sh
-    cp -fr $f $fn
-    sh $f
-    rm -f $f
+    cp -fr $f $fn && rm -fr $f
+    sh $fn
+    rm -fr $fn
   fi
 done
