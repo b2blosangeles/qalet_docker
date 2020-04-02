afab96e4ff42801995cb37ca6433b4b79c2d2953
b2blosangeles <b2blosangeles@gmail.com>
2020-04-01 21:10:44 -0700

    Update appRouter.js

--- qalet_admin/modules/appRouter.js
+++ qalet_admin/modules/appRouter.js
@@ -236,7 +236,7 @@
 						me.runScript(v[1]);
 						break;
 					case 'api':
-						res.render('html/index.ect', {( module: "api 66 get"});
+						res.renderr('html/index.ect', { module: "api 66 get"});
 						break;	
 					case 'checkip':
 						res.render('html/index.ect', { module: "checkip"});
