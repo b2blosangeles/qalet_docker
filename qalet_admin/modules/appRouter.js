9f165053347070b2478ab9c22321806777cc575e
b2blosangeles <b2blosangeles@gmail.com>
2020-04-07 12:45:33 -0700

    Update appRouter.js

--- qalet_admin/modules/appRouter.js
+++ qalet_admin/modules/appRouter.js
@@ -14,35 +14,35 @@
 			res.end();			
 		}
 		this.vhosts = [
-					{
-						serverName	: 'www.shusiou.win', 
-						serverAlias	: 'shusiou.win',
-						gitHub		: 'https://github.com/b2blosangeles/docker_apachePHP.git',
-						innerPort	: 80,
-						gatewayIp	: '173.28.5.254',
-						gatewayPort: 20001
-					},
-					{
-						serverName	: 'wwwe.shusiou.win', 
-						gitHub		: 'https://github.com/b2blosangeles/docker_apachePHP.git',
-						innerPort	: 80,
-						gatewayIp	: '173.28.5.254',
-						gatewayPort: 20005
-					},
-					{
-						serverName	: 'ss1.shusiou.win', 
-						gitHub		: 'https://github.com/b2blosangeles/docker_apachePHP.git',
-						innerPort	: 80,
-						gatewayIp	: '173.28.5.254',
-						gatewayPort: 20010
-					},
-					{
-						serverName	: 'ss2.shusiou.win', 
-						gitHub		: 'https://github.com/b2blosangeles/docker_apachePHP.git',
-						innerPort	: 80,
-						gatewayIp	: '173.28.5.254',
-						gatewayPort: 20020
-					}
+			{
+				serverName	: 'www.shusiou.win', 
+				serverAlias	: 'shusiou.win',
+				gitHub		: 'https://github.com/b2blosangeles/docker_apachePHP.git',
+				innerPort	: 80,
+				gatewayIp	: '173.28.5.254',
+				gatewayPort: 20001
+			},
+			{
+				serverName	: 'wwwe.shusiou.win', 
+				gitHub		: 'https://github.com/b2blosangeles/docker_apachePHP.git',
+				innerPort	: 80,
+				gatewayIp	: '173.28.5.254',
+				gatewayPort: 20005
+			},
+			{
+				serverName	: 'ss1.shusiou.win', 
+				gitHub		: 'https://github.com/b2blosangeles/docker_apachePHP.git',
+				innerPort	: 80,
+				gatewayIp	: '173.28.5.254',
+				gatewayPort: 20010
+			},
+			{
+				serverName	: 'ss2.shusiou.win', 
+				gitHub		: 'https://github.com/b2blosangeles/docker_apachePHP.git',
+				innerPort	: 80,
+				gatewayIp	: '173.28.5.254',
+				gatewayPort: 20020
+			}
 				];
 		this.addHost = function() {
 			var me = this;
