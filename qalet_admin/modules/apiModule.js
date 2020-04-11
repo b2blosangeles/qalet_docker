72ec0b1f93e97a746325abbdd13b82b53446e8f5
b2blosangeles <b2blosangeles@gmail.com>
2020-04-11 11:10:30 -0700

    Update apiModule.js

--- qalet_admin/modules/apiModule.js
+++ qalet_admin/modules/apiModule.js
@@ -3,6 +3,53 @@
 		this.do = function(cmd, spacename) {
 			res.send(env);
 		}
+		this.addDB = function() {
+			var me = this;
+			var CP = new pkg.crowdProcess(),_f = {}; 
+
+			_f['prepare_folder'] = function(cbk) {
+				var cmd = 'mkdir -p ' + env.root + '/db_setting';
+				exec(cmd, 
+				     {maxBuffer: 1024 * 2048},
+				     function(error, stdout, stderr) {
+					cbk(true);
+				});
+			}
+			
+			_f['savefile'] = function(cbk) {
+				var dt = req.body;
+				dt.gitHub = 'https://github.com/b2blosangeles/docker_mysql.git';
+				var data = [];
+				try {
+					delete require.cache[env.root + '/db_setting/dbs.json'];
+					data = require(env.root + '/db_setting/dbs.json');
+				} catch(e) {
+					
+				} 				
+				data.push(dt);
+				
+				fs.writeFile(env.root + '/db_setting/dbs.json', JSON.stringify(data), (err) => {
+				  cbk(true);
+				});
+			}
+			
+			CP.serial(
+				_f,
+				function(data) {
+					res.writeHead(301,
+					  {Location: 'http://admin.shusiou.win/dbs'}
+					);
+					res.end();
+										/*
+					res.render('html/frame.ect', {module:'dbs', 
+						dbs : me.dbs,		      
+						data : JSON.stringify(data)
+					});*/
+			   	},
+			   	6000
+			);
+			
+		}
 	};
 
 	if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
