a40dedfba324637e4ee9e0d43db55b3c13e5a7bd
b2blosangeles <b2blosangeles@gmail.com>
2020-04-11 16:08:52 -0700

    Update apiModule.js

--- qalet_admin/modules/apiModule.js
+++ qalet_admin/modules/apiModule.js
@@ -4,6 +4,48 @@
 		this.do = function(cmd, spacename) {
 			res.send(env);
 		}
+		this.removeDB = function() {
+			var me = this;
+			var CP = new pkg.crowdProcess(),_f = {}; 
+		
+			_f['prepare_folder'] = function(cbk) {
+				var cmd = 'mkdir -p ' + env.root + '/db_setting';
+				pkg.exec(cmd, 
+				     {maxBuffer: 1024 * 2048},
+				     function(error, stdout, stderr) {
+					cbk(true);
+				});
+			}
+			
+			_f['savefile'] = function(cbk) {
+				var dt = req.body;
+				dt.gitHub = 'https://github.com/b2blosangeles/docker_mysql.git';
+				var data = [];
+				try {
+					delete require.cache[env.root + '/db_setting/dbs.json'];
+					data = require(env.root + '/db_setting/dbs.json');
+				} catch(e) {
+					
+				} 				
+				data.push(dt);
+				
+				pkg.fs.writeFile(env.root + '/db_setting/dbs.json', JSON.stringify(data), (err) => {
+				  cbk(true);
+				});
+			}
+			
+			CP.serial(
+				_f,
+				function(data) {
+					res.writeHead(301,
+					  {Location: 'http://admin.shusiou.win/dbs'}
+					);
+					res.end();
+			   	},
+			   	6000
+			);
+			
+		}		
 		this.addDB = function() {
 			var me = this;
 			var CP = new pkg.crowdProcess(),_f = {}; 
