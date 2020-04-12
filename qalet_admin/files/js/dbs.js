5907121e7e12149e754d4bb5ae921c11d7209474
b2blosangeles <b2blosangeles@gmail.com>
2020-04-11 17:27:12 -0700

    Update dbs.js

--- qalet_admin/files/js/dbs.js
+++ qalet_admin/files/js/dbs.js
@@ -1,6 +1,8 @@
 if (!_qalet) var _qalet = {};
 $(document).ready(function(){
     _qalet.submitAddDB = function() {
+        let formData = $('#addMySQLDBFrom').serializeObject();
+        console.log(formData);
         alert('welcome dbs.js');
     }
     
