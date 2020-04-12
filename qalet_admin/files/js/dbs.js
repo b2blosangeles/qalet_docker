3e3d521b4af86cd4fc77c3d1c697e442500da2ca
b2blosangeles <b2blosangeles@gmail.com>
2020-04-11 17:51:57 -0700

    Update dbs.js

--- qalet_admin/files/js/dbs.js
+++ qalet_admin/files/js/dbs.js
@@ -1,4 +1,24 @@
 if (!_qalet) var _qalet = {};
+
+(function ($) {
+    $.fn.serializeFormJSON = function () {
+
+        var o = {};
+        var a = this.serializeArray();
+        $.each(a, function () {
+            if (o[this.name]) {
+                if (!o[this.name].push) {
+                    o[this.name] = [o[this.name]];
+                }
+                o[this.name].push(this.value || '');
+            } else {
+                o[this.name] = this.value || '';
+            }
+        });
+        return o;
+    };
+})(jQuery);
+
 $(document).ready(function(){
     _qalet.submitAddDB = function() {
         let formData = $('#addMySQLDBFrom').serializeObject();
