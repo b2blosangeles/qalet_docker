7b7e269d15dfe16fa56a17632d78af8257a6392d
b2blosangeles <b2blosangeles@gmail.com>
2020-04-17 08:40:57 -0700

    Create vueCommon.js

---
+++ qalet_admin/files/js/vueCommon.js
@@ -0,0 +1,5 @@
+var vueCommon = {
+    A : httpVueLoader('/commLib/vue/componentA.vue'),
+    B : httpVueLoader('/commLib/vue/componentB.vue'),
+    C : httpVueLoader('/commLib/vue/componentC.vue')
+}
