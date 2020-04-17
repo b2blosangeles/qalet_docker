6a639047b76e3ec11b0395cbcab879e658d1e86d
b2blosangeles <b2blosangeles@gmail.com>
2020-04-17 14:53:07 -0700

    Update vueCommon.js

--- qalet_admin/httpdocs/js/vueCommon.js
+++ qalet_admin/httpdocs/js/vueCommon.js
@@ -1,7 +1,7 @@
 let code = `<template><section><h1>SFC string.</h1></template><script>module.exports = {computed: {random() {return Math.random()}}}<\/script>`;
 
 var vueCommon = {
-    A : codeSFCLoader('/commLib/vue/componentA.vue'),
-    B : codeSFCLoader(code),
-    C : codeSFCLoader('/commLib/vue/componentC.vue')
+    A : codeVeuSFCLoader('/commLib/vue/componentA.vue'),
+    B : codeVeuSFCLoader(code),
+    C : codeVeuSFCLoader('/commLib/vue/componentC.vue')
 }
