04d6f40a804bc9191c42d69211f2c9772301a75e
b2blosangeles <b2blosangeles@gmail.com>
2020-04-17 13:52:28 -0700

    Update vueCommon.js

--- qalet_admin/httpdocs/js/vueCommon.js
+++ qalet_admin/httpdocs/js/vueCommon.js
@@ -1,5 +1,12 @@
+let code = `<template><section><h1>This component has been mounted dynamically using <em>http-vue-loader</em> and a harcoded SFC string.</h1><p>Here’s a random number from the component: {{ random }}</p></section></template><script>module.exports = {computed: {random() {return Math.random()}}}<\/script>`;
+httpVueLoader.httpRequest = function(code) {
+  return new Promise(function(resolve, reject) {
+    resolve(code);
+  });
+};
 var vueCommon = {
-    A : '<span>kkk</span>',
-    B : httpVueLoader('/commLib/vue/componentB.vue'),
-    C : httpVueLoader('/commLib/vue/componentC.vue')
+    A : httpVueLoader(code),
+    B : httpVueLoader(code)
+
 }
+//     C : httpVueLoader('/commLib/vue/componentC.vue')
