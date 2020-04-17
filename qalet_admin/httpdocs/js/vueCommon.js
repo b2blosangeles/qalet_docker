b04b240381f9f199bebfd404e9f9f9023995ae1b
b2blosangeles <b2blosangeles@gmail.com>
2020-04-17 14:31:31 -0700

    Update vueCommon.js

--- qalet_admin/httpdocs/js/vueCommon.js
+++ qalet_admin/httpdocs/js/vueCommon.js
@@ -1,12 +1,8 @@
 let code = `<template><section><h1>SFC string.</h1></template><script>module.exports = {computed: {random() {return Math.random()}}}<\/script>`;
-httpVueLoader.httpRequest = function(code) {
-  return new Promise(function(resolve, reject) {
-    resolve(code);
-  });
-};
-var vueCommon = {
-    A : httpVueLoader(code),
-    B : httpVueLoader(code)
 
+var vueCommon = {
+    A : codeVueLoader(code),
+    B : codeVueLoader('/commLib/vue/componentB.vue'),
+    C : codeVueLoader('/commLib/vue/componentC.vue')
 }
 //     C : httpVueLoader('/commLib/vue/componentC.vue')
