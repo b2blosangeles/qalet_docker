a221df90b790a466cb9d979ff6d5553b42c819b3
b2blosangeles <b2blosangeles@gmail.com>
2020-04-16 19:33:32 -0700

    Update my-component.vue

--- qalet_admin/files/vue/my-component.vue
+++ qalet_admin/files/vue/my-component.vue
@@ -1,9 +1,10 @@
 <template>
-    <div class="hello">Hello {{who}}</div>
+    <div class="hello">Hello {{postTitle}}</div>
 </template>
  
 <script>
 module.exports = {
+    props: ['postTitle'],
     data: function() {
         return {
             who: 'world'
