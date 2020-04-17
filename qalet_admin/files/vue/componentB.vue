79abc5e35642543a34ecc16180d623cdde21eb77
b2blosangeles <b2blosangeles@gmail.com>
2020-04-16 21:40:23 -0700

    Create componentB.vue

---
+++ qalet_admin/files/vue/componentB.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="hello">Hello B {{postTitle}}</div>
+</template>
+ 
+<script>
+module.exports = {
+    props: ['postTitle'],
+    data: function() {
+        return {
+            who: 'world'
+        }
+    }
+}
+</script>
+<style>
+.hello {
+    background-color: #ffe;
+}
+</style>
