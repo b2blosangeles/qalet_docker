d08fca7281850b36580652bf80e9ef76a2be46b1
b2blosangeles <b2blosangeles@gmail.com>
2020-04-18 16:01:35 -0700

    Update cleanRemovedFiles.sh

--- qalet_admin/localCron/cleanRemovedFiles.sh
+++ qalet_admin/localCron/cleanRemovedFiles.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 [ -z "$1" -o -z "$2" ] && {  ## validate input
-    printf "error: insufficient input. Usage: %s tmpfiles storage\n" ${0//*\//}
+    echo "error: insufficient input. Usage: %s tmpfiles storage\n" ${0//*\//}
     exit 1
 }
 
@@ -11,6 +11,6 @@ for i in "$1"/*; do
     ## if file in backup matches filename, skip rest of loop
     ls ${2}* | grep -q $fn &>/dev/null && continue
 
-    printf "removing %s\n" "$i"
+    echo "removing %s\n" "$i"
     # rm "$i" ## remove file
 done
