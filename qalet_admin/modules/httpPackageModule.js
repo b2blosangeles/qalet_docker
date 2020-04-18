61c846f983f6392eddcba5e6e7abe7bf3e4d52cf
b2blosangeles <b2blosangeles@gmail.com>
2020-04-17 23:05:21 -0700

    Update httpPackageModule.js

--- qalet_admin/modules/httpPackageModule.js
+++ qalet_admin/modules/httpPackageModule.js
@@ -61,9 +61,9 @@
 						let fileName = lfn.substring(lfn.lastIndexOf('/')+1).replace(/\..*$/,' ');
 						
 						str += 'vueCommon.' + fileName + ' = ';
-						str += "codeVeuSFCLoader(decodeURIComponent(`" +  
+						str += "codeVeuSFCLoader(`" +  
 							CP.data['_' + i].replace(/(\r\n|\n|\r)/gm,' ') +  
-						"`)); \n";
+						"`); \n";
 						str += 'console.log(vueCommon.' + fileName + '.toString()); ' + "\n";
 					       // str += 'vueCommon.' + fileName + ' = ';
 						// str += codeVeuSFCLoader()(decodeURIComponent('`' + CP.data['_' + i].replace(/(\r\n|\n|\r)/gm,' ') + '`'));
