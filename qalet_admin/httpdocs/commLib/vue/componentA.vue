a31ed4912cd114013649ab8b6b2ff36073c58412
b2blosangeles <b2blosangeles@gmail.com>
2020-04-17 19:06:16 -0700

    Update componentA.vue

--- qalet_admin/httpdocs/commLib/vue/componentA.vue
+++ qalet_admin/httpdocs/commLib/vue/componentA.vue
@@ -1,1 +1,1 @@
-<template><section><h1>SFC 5 string.</h1></template><script>module.exports = {computed: {random() {return Math.random()}}}<\/script>
+<template><section><h1>SFC 5 string.</h1></template><script>module.exports = {computed: {random() {return Math.random()}}}</script>
