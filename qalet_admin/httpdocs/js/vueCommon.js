aa6afd03b793afc65d1891e51f1302a8f5ec9d20
b2blosangeles <b2blosangeles@gmail.com>
2020-04-18 11:11:40 -0700

    Delete vueCommon.js

--- qalet_admin/httpdocs/js/vueCommon.js
+++
@@ -1,8 +0,0 @@
-// var code = `<template><section><h1>SFC niu string.</h1></template><script>module.exports = {computed: {random() {return Math.random()}}}<\/script>`;
-/*
-var vueCommon = {
-    componentA : codeVeuSFCLoader('/commLib/vue/componentA.vue'),
-    componentB : codeVeuSFCLoader(code),
-    componentC : codeVeuSFCLoader('/commLib/vue/componentC.vue')
-}
-*/
