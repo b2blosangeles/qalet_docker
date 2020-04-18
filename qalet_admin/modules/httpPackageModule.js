96db2aa3ba027560763ba45618a18eca4658e1c0
b2blosangeles <b2blosangeles@gmail.com>
2020-04-18 07:49:25 -0700

    Update httpPackageModule.js

--- qalet_admin/modules/httpPackageModule.js
+++ qalet_admin/modules/httpPackageModule.js
@@ -20,89 +20,7 @@
 			      }
 			});
 		}
-		this.removeComments = function(str) {
-		    str = ('__' + str + '__').split('');
-		    var mode = {
-			singleQuote: false,
-			doubleQuote: false,
-			regex: false,
-			blockComment: false,
-			lineComment: false,
-			condComp: false 
-		    };
-		    for (var i = 0, l = str.length; i < l; i++) {
 
-			if (mode.regex) {
-			    if (str[i] === '/' && str[i-1] !== '\') {
-				mode.regex = false;
-			    }
-			    continue;
-			}
-
-			if (mode.singleQuote) {
-			    if (str[i] === "'" && str[i-1] !== '\') {
-				mode.singleQuote = false;
-			    }
-			    continue;
-			}
-
-			if (mode.doubleQuote) {
-			    if (str[i] === '"' && str[i-1] !== '\') {
-				mode.doubleQuote = false;
-			    }
-			    continue;
-			}
-
-			if (mode.blockComment) {
-			    if (str[i] === '*' && str[i+1] === '/') {
-				str[i+1] = '';
-				mode.blockComment = false;
-			    }
-			    str[i] = '';
-			    continue;
-			}
-
-			if (mode.lineComment) {
-			    if (str[i+1] === 'n' || str[i+1] === 'r') {
-				mode.lineComment = false;
-			    }
-			    str[i] = '';
-			    continue;
-			}
-
-			if (mode.condComp) {
-			    if (str[i-2] === '@' && str[i-1] === '*' && str[i] === '/') {
-				mode.condComp = false;
-			    }
-			    continue;
-			}
-
-			mode.doubleQuote = str[i] === '"';
-			mode.singleQuote = str[i] === "'";
-
-			if (str[i] === '/') {
-
-			    if (str[i+1] === '*' && str[i+2] === '@') {
-				mode.condComp = true;
-				continue;
-			    }
-			    if (str[i+1] === '*') {
-				str[i] = '';
-				mode.blockComment = true;
-				continue;
-			    }
-			    if (str[i+1] === '/') {
-				str[i] = '';
-				mode.lineComment = true;
-				continue;
-			    }
-			    mode.regex = true;
-
-			}
-
-		    }
-		    return str.join('').slice(2, -2);
-		}
 		this.veuFiles = function(list) {
 			var me = this;
 			var CP = new pkg.crowdProcess(),_f = {}; 
@@ -115,7 +33,7 @@
 			_f['codeVeuSFCLoader'] = function(cbk) {
 				let lfn = dirn + 'js/codeVeuSFCLoader.js';
 				pkg.fs.readFile(lfn, 'utf8', function(err, data){
-					cbk(me.removeComments(data));
+					cbk(data);
 				}); 
 				return true;
 			}	
