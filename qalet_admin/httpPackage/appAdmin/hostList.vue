50928ed04b4f7cdae538eb9bda8987eaadf44ed2
b2blosangeles <b2blosangeles@gmail.com>
2020-04-19 14:36:04 -0700

    Update hostList.vue

--- qalet_admin/httpPackage/appAdmin/hostList.vue
+++ qalet_admin/httpPackage/appAdmin/hostList.vue
@@ -29,7 +29,7 @@ module.exports = {
     props: ["postTitle"],
     data: function() {  
         return {
-            currentModule : null,
+            currentModule : '',
             items : []
         }
     },
