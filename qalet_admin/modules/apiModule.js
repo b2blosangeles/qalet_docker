ba7fb0d9f7a2966a96e94e2d9a9830d09f659e29
b2blosangeles <b2blosangeles@gmail.com>
2020-04-19 10:47:28 -0700

    Update apiModule.js

--- qalet_admin/modules/apiModule.js
+++ qalet_admin/modules/apiModule.js
@@ -1,5 +1,23 @@
 (function () { 
 	var obj =  function (env, pkg, req, res) {
+		this.vhosts = [
+			{
+				serverName	: 'www.shusiou.winA', 
+				serverAlias	: 'shusiou.win',
+				gitHub		: 'https://github.com/b2blosangeles/docker_apachePHP.git',
+				innerPort	: 80,
+				gatewayIp	: '173.28.5.254',
+				gatewayPort: 20001
+			},
+			{
+				serverName	: 'www.shusiou.winB', 
+				serverAlias	: 'shusiou.win',
+				gitHub		: 'https://github.com/b2blosangeles/docker_apachePHP.git',
+				innerPort	: 80,
+				gatewayIp	: '173.28.5.254',
+				gatewayPort: 20001
+			}
+		];
 		this.get = function(cmd) {
 			res.send(cmd);
 		}
