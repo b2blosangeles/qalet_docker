508cbd69ff17624cd45eefac0a9ec26ff25d3cdf
b2blosangeles <b2blosangeles@gmail.com>
2020-04-18 20:54:39 -0700

    Update dbList.vue

--- qalet_admin/httpPackage/appAdmin/dbList.vue
+++ qalet_admin/httpPackage/appAdmin/dbList.vue
@@ -34,7 +34,7 @@ module.exports = {
                 {
                     serverName	: 'www.shusiou.win', 
                     serverAlias	: 'shusiou.win',
-                    "gitHub" : "github.com/b2blosangeles\/docker_apachePHP.git",
+                    "gitHub" : "http://github.com/b2blosangeles/docker_apachePHP.git",
                     innerPort	: '80',
                     gatewayIp	: '173.28.5.254',
                     gatewayPort: '20001'
