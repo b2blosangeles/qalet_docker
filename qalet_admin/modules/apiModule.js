37d66503cc95026b571cb8d3866dcce4864d713f
b2blosangeles <b2blosangeles@gmail.com>
2020-04-19 13:51:22 -0700

    Update apiModule.js

--- qalet_admin/modules/apiModule.js
+++ qalet_admin/modules/apiModule.js
@@ -1,5 +1,23 @@
 (function () { 
 	var obj =  function (env, pkg, req, res) {
+		this.dbs = [
+			{
+				serverName	: 'www.shusiou.winADB', 
+				serverAlias	: 'shusiou.win',
+				gitHub		: 'https://github.com/b2blosangeles/docker_apachePHP.git',
+				innerPort	: 80,
+				gatewayIp	: '173.28.5.254',
+				gatewayPort: 20001
+			},
+			{
+				serverName	: 'www.shusiou.winBDB', 
+				serverAlias	: 'shusiou.win',
+				gitHub		: 'https://github.com/b2blosangeles/docker_apachePHP.git',
+				innerPort	: 80,
+				gatewayIp	: '173.28.5.254',
+				gatewayPort: 20001
+			}
+		];
 		this.vhosts = [
 			{
 				serverName	: 'www.shusiou.winA', 
@@ -18,13 +36,32 @@
 				gatewayPort: 20001
 			}
 		];
-		this.get = function(cmd) {
-			res.send({
-				status	: 'success',
-				cmd 	: cmd,
-				results	: this.vhosts
-				
-			});
+		this.get = function(code) {
+			switch(code) {
+				case 'dbs'	:
+					res.send({
+						status	: 'success',
+						cmd 	: code,
+						results	: this.dbs
+
+					});
+					break;
+				case 'hosts'	:
+					res.send({
+						status	: 'success',
+						cmd 	: code,
+						results	: this.vhosts
+
+					});
+					break;
+				default : 	
+					res.send({
+						status	: 'failure',
+						cmd 	: code,
+						message	: 'Missiing or wrong code'
+					});		
+			}
+
 		}
 	};
 
