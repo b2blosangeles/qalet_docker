3071dcb7532a3be874447404bce72eab6c43d0dc
b2blosangeles <b2blosangeles@gmail.com>
2020-04-18 19:56:00 -0700

    Update dbList.vue

--- qalet_admin/httpPackage/appAdmin/dbList.vue
+++ qalet_admin/httpPackage/appAdmin/dbList.vue
@@ -1,7 +1,20 @@
 <template>
     <span>
         <h3>QALET Databases</h3>
-        <div class="db_list_class">DB list 22</div>
+        <!--div class="db_list_class">DB list 22</div-->
+         <table class="table">
+            <thead>
+              <tr>
+                <th>DB Name</th>
+                <th>ip address</th>
+                <th>Port</th>
+                <th>Gateway:Port</th>
+                <th><a href="/addMySQLDB"><button type="button" class="btn btn-warning"><i class="icon-plus-sign-alt"></i> Add</button></a></th>
+              </tr>
+            </thead>
+            <tbody>
+            </tbody>
+          </table>
      </span>
 </template>
  
