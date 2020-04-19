ccbd4b23fb6641878498eb75c152e70450b5aaed
b2blosangeles <b2blosangeles@gmail.com>
2020-04-18 21:46:29 -0700

    Update httpPackageModule.js

--- qalet_admin/modules/httpPackageModule.js
+++ qalet_admin/modules/httpPackageModule.js
@@ -132,7 +132,7 @@
 					return function(cbk) {
 						let lfn =  _folder + '/' + list[i].replace(/^\//, '');
 						pkg.fs.readFile(lfn, 'utf8', function(err, data){
-							data = (err) ? '' : data;
+							data = (err) ? '' : me.removeComments(data);
 							// me.removeComments(data);
 							cbk(encodeURIComponent(data.replace(/(\r\n|\n|\r)/gm,' '))); 
 						}); 
