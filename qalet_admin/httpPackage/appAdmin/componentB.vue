5e049839e7dbb15c7f199e8877f8914ea821f6a8
b2blosangeles <b2blosangeles@gmail.com>
2020-04-19 17:41:43 -0700

    Update componentB.vue

--- qalet_admin/httpPackage/appAdmin/componentB.vue
+++ qalet_admin/httpPackage/appAdmin/componentB.vue
@@ -1,19 +1,19 @@
 <template>
-  <div>=={{postTitle}}==</div>
+    <div class="helloB">Hello {{postTitle}}</div>
 </template>
-
+ 
 <script>
-
-  module.exports = {
-      props: ["postTitle"],
-      data: function() {
-          return {
-              who: 'world'
-          }
-      }
-  }
+module.exports = {
+    props: ["postTitle"],
+    data: function() {
+        return {
+            who: 'world'
+        }
+    }
+}
 </script>
-
 <style>
-
+.helloB {
+    background-color: green;
+}
 </style>
