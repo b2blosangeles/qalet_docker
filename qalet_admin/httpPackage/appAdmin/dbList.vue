7bb96cc264060701dde560aa21f9c3bb3349a0e1
b2blosangeles <b2blosangeles@gmail.com>
2020-04-22 09:52:21 -0700

    Update dbList.vue

--- qalet_admin/httpPackage/appAdmin/dbList.vue
+++ qalet_admin/httpPackage/appAdmin/dbList.vue
@@ -2,7 +2,7 @@
     <span>
         <message-section-a postTitle="niu bi"></message-section-a>
         <h3>QALET Databases</h3>
-         <spinner spinner="spinner"></spinner>
+         <spinner trigger="spinner"></spinner>
          <table class="table" v-if="currentAction==''">
             <thead>
               <tr>
