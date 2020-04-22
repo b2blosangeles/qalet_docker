23eeab96798d31df76f6b0741562ce81a274140d
b2blosangeles <b2blosangeles@gmail.com>
2020-04-22 11:30:47 -0700

    Update spinner.vue

--- qalet_admin/httpPackage/appAdmin/spinner.vue
+++ qalet_admin/httpPackage/appAdmin/spinner.vue
@@ -1,6 +1,5 @@
 <template>
     <span>
-    ={{typeof trigger}}={{ trigger}}
     <div class="overlay_spin_cover" v-if="trigger"><div class="section-spinner"></div></div></div>
     </span>
 </template>
