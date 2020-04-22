95d4e9da87e9991c1ec6f00102aaae460798b0b5
b2blosangeles <b2blosangeles@gmail.com>
2020-04-22 11:22:42 -0700

    Update spinner.vue

--- qalet_admin/httpPackage/appAdmin/spinner.vue
+++ qalet_admin/httpPackage/appAdmin/spinner.vue
@@ -1,7 +1,7 @@
 <template>
     <span>
     ={{typeof spinnertrigger}}={{ spinnertrigger}}
-    <div class="overlay_spin_cover" v-if="spinnertrigger=='on'"><div class="section-spinner"></div></div></div>
+    <div class="overlay_spin_cover" v-if="spinnertrigger"><div class="section-spinner"></div></div></div>
     </span>
 </template>
  
