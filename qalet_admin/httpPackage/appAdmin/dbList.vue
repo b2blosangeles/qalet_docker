2955801881e309f44f0bb864370be790fa1f5d05
b2blosangeles <b2blosangeles@gmail.com>
2020-04-23 14:57:07 -0700

    Update dbList.vue

--- qalet_admin/httpPackage/appAdmin/dbList.vue
+++ qalet_admin/httpPackage/appAdmin/dbList.vue
@@ -1,7 +1,7 @@
 <template>
     <span>
         <pop-up>test</pop-up>
-        <h3>QALET Databases 2</h3>
+        <h3>QALET Databases 3</h3>
         <data-engine  v-bind:config="dataEngineConfig"  v-bind:id="dataEngineConfig.id" v-bind:result="resultData"></data-engine>
          <table class="table" v-if="currentAction==''">
             <thead>
