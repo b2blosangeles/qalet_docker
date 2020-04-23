f218c7c98aa4ed70971b3506561aaa1540112beb
b2blosangeles <b2blosangeles@gmail.com>
2020-04-22 23:11:49 -0700

    Update hostList.vue

--- qalet_admin/httpPackage/appAdmin/hostList.vue
+++ qalet_admin/httpPackage/appAdmin/hostList.vue
@@ -52,9 +52,9 @@ module.exports = {
     },
     mounted ()  {
         console.log("==mounted==");
-     // setTimeout(function() {
+        setTimeout(function() {
             me.loadData();
-      //  }, 3000); 
+        }); 
     },
     methods : {
         setAction(v) {
