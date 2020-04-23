cad9dc9338c28841ba4f97855f5bc577cdcc2943
b2blosangeles <b2blosangeles@gmail.com>
2020-04-22 19:17:40 -0700

    Update dataEngine.vue

--- qalet_admin/httpPackage/appAdmin/dataEngine.vue
+++ qalet_admin/httpPackage/appAdmin/dataEngine.vue
@@ -1,4 +1,5 @@
 <template>
+    <spinner v-bind:['trigger']="spinnerTrigger"></spinner>
     <span>
         test-->--{{config.url}}--==-test-dataEngine
     </span>
@@ -16,10 +17,15 @@ module.exports = {
                 console.log('<---------');
                 this.config.id = 0;
                 this.result.respId = newVal;
+                loadItems();
             }
         }
     },
     data: {
+    },
+    components : {
+        spinner : QALETCOMM.spinner,
+        dataEngine : QALETCOMM.dataEngine
     },
     created ()  {
         console.log(this.showConfig());
@@ -29,6 +35,17 @@ module.exports = {
         showConfig () {
            if (typeof config == 'undefined') return '-- created --';  
            else config.url;
+        },
+        loadItems() {
+            this.spinnerTrigger = true;
+            this.$http.post('/api', {code: 'vhosts'}).then(response => {
+               this.spinnerTrigger = false;
+               this.items = response.body.results;
+                console.log(response.body);
+            }, response => {
+                this.spinnerTrigger = false;    
+                console.log('--error---');
+            });
         }
     }
 }
