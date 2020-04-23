c45fa1976708f5a3154ca18a13e3586d6a2beecc
b2blosangeles <b2blosangeles@gmail.com>
2020-04-22 19:38:58 -0700

    Update hostList.vue

--- qalet_admin/httpPackage/appAdmin/hostList.vue
+++ qalet_admin/httpPackage/appAdmin/hostList.vue
@@ -3,6 +3,7 @@
         <h3>QALET Virtual Hosts</h3>
 
         <data-engine  v-bind:config="dataEngineConfig"  v-bind:id="dataEngineConfig.id" v-bind:result="resultData"></data-engine>
+        ---->>>
         <hr/>
         <button type="button" class="btn btn-warning"  v-on:click="dataEngineConfig.id = new Date().getTime()">Call Engine</button>
         --{{dataEngineConfig}}--==={{resultData}}===
