f6ae48446488b455fe41669894953a17abd76a13
b2blosangeles <b2blosangeles@gmail.com>
2020-04-22 22:07:36 -0700

    Update dataEngine.vue

--- qalet_admin/httpPackage/appAdmin/dataEngine.vue
+++ qalet_admin/httpPackage/appAdmin/dataEngine.vue
@@ -46,7 +46,6 @@ module.exports = {
               // }
             //   this.result.items = [1, 2];
                 console.log('--this.result--->');
-                console.log(obj);
                // console.log(response.body.results);
             }, response => {
                 this.spinnerTrigger = false;    
