5e3afa7019315c13cba5a309f34e236e42b4e160
b2blosangeles <b2blosangeles@gmail.com>
2020-04-23 16:06:15 -0700

    Update popUp.vue

--- qalet_admin/httpPackage/appAdmin/popUp.vue
+++ qalet_admin/httpPackage/appAdmin/popUp.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <div class="overlay_popup_cover"></div>
-        <div class="overlay_popup"> trt </div>
+        <div class="overlay_popup"><div class="popupWindow"> trt </div></div>
     </div>    
 </template>
 
