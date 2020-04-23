2325956648419701a82db5e64b33a2f9f30cef92
b2blosangeles <b2blosangeles@gmail.com>
2020-04-22 23:38:17 -0700

    Update dataEngine.vue

--- qalet_admin/httpPackage/appAdmin/dataEngine.vue
+++ qalet_admin/httpPackage/appAdmin/dataEngine.vue
@@ -31,9 +31,9 @@ module.exports = {
            this.spinnerTrigger = true;
            var me = this;
            // var ajax =  (!config.postData) ? this.$http.get(config.uri) : this.$http.post(config.uri, config.postData);
-           // var ajax =  (!config.postData) ? this.$http.get(config.uri) : this.$http.post(config.uri, config.postData);
-           // ajax.then(function (response) {
-            this.$http.post(this.config.uri, this.config.postData).then(function (response) {
+            var ajax =  (!this.config.postData) ? this.$http.get(this.config.uri) : this.$http.post(this.config.uri, this.config.postData);
+            ajax.then(function (response) {
+          //  this.$http.post(this.config.uri, this.config.postData).then(function (response) {
                 this.result.items = response.body.results;
                 this.spinnerTrigger = false;
                 me.config.id = 0;
