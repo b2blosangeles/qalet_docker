7413e534c2c33af051eeba8cc010ec38b48d88d7
b2blosangeles <b2blosangeles@gmail.com>
2020-04-23 15:28:51 -0700

    Update popUp.vue

--- qalet_admin/httpPackage/appAdmin/popUp.vue
+++ qalet_admin/httpPackage/appAdmin/popUp.vue
@@ -11,5 +11,27 @@ module.exports = {
 }
 </script>
 <style>
-
+.overlay_popup {
+    background: transparent; position: fixed;
+    height: 100%; width: 100%;
+    z-index: 5001;
+    top: 0; left: 0;
+    float: left;
+    text-align: center;
+    padding-top: 6em;
+    opacity: 1
+}
+.overlay_popup_cover {
+    position:fixed;
+    top:0;
+    left:0;
+    background:rgba(200,200,200,0.8);
+    z-index:5000;
+    width:100%;
+    height:100%;
+}
+.popupWindow {
+    background-color: #ff0000;
+    width : 640px;
+}
 </style>
