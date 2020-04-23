305e8bba907b5512556cc2f4ebd358c3d3d05c0f
b2blosangeles <b2blosangeles@gmail.com>
2020-04-23 15:40:40 -0700

    Update dbList.vue

--- qalet_admin/httpPackage/appAdmin/dbList.vue
+++ qalet_admin/httpPackage/appAdmin/dbList.vue
@@ -24,7 +24,7 @@
             </tbody>
           </table>
           
-          <ppp-up>test</ppp-up>
+          <pop-up>test</pop-up>
                  
           <div v-if="currentAction!=''">
             <button type="button" class="btn btn-warning"  v-on:click="setAction('')"><i class="icon-plus-sign-alt"></i> Cancel</button>
