5ec56cbff44f11249c9c714569c5246af67a1a33
b2blosangeles <b2blosangeles@gmail.com>
2020-04-23 08:18:11 -0700

    Update spinner.vue

--- qalet_admin/httpPackage/appAdmin/spinner.vue
+++ qalet_admin/httpPackage/appAdmin/spinner.vue
@@ -1,6 +1,6 @@
 <template>
     <span v-if="trigger">
-        <!--div class="overlay_spin_cover" v-if="trigger"><div class="section-spinner"></div></div-->
+        <div class="overlay_spin_cover" v-if="!config.localSpinner"><div class="section-spinner"></div></div>
         <div class="section_spin_cover" v-if="config.localSpinner"><div class="section-spinner"></div></div>
         <!--div class="spinner" v-if="trigger"></div-->
         
