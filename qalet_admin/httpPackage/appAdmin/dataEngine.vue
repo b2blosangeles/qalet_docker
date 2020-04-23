5ae53340f2206e6d0b990667419b2b0075678bc6
b2blosangeles <b2blosangeles@gmail.com>
2020-04-22 22:50:29 -0700

    Update dataEngine.vue

--- qalet_admin/httpPackage/appAdmin/dataEngine.vue
+++ qalet_admin/httpPackage/appAdmin/dataEngine.vue
@@ -11,11 +11,7 @@ module.exports = {
     watch: { 
       	id: function(newVal, oldVal) { 
             if (newVal) {
-               // this.config.id = 0;
-              //  this.result.respId = newVal;
-                
-              //   this.result.items = [{serverName: "www.shusiou.winAPOSTb", serverAlias: "shusiou.win"}, {serverName: "www.shusiou.winAPOSTa", serverAlias: "shusiou.win"}];
-                this.loadItems(this);
+                this.loadItems(newVal);
             }
           }
     },
@@ -36,46 +32,17 @@ module.exports = {
            if (typeof config == 'undefined') return '-- created --';  
            else config.url;
         },
-        loadItems(o) {
+        loadItems(id) {
            this.spinnerTrigger = true;
            var me = this;
-
-           
             this.$http.post('/api', {code: 'vhosts'}). then(function (response) {
-                
                 this.result.items = response.body.results;
-                me.result.respId = '8888888';
-                me.config.id = 0;
-                console.log(this.result);
+                me.result.respId = id;
                 this.spinnerTrigger = false;
+                me.config.id = 0;
             }).catch((err) => {
               console.log(err)
             });
-           /*
-           me.$http.post('/api', {code: 'vhosts'}).then(
-            
-                (function(me) {
-                    return response => {
-                   me.spinnerTrigger = false;
-                  
-                   me.result.respId = '12345';
-                   
-                    o.result.respId = '8888888';
-                    
-                   console.log(me.result);
-                  // for (var i=0; i < response.body.results.length; i++) {
-                  //         this.result.items.push(response.body.results[i]);
-                  // }
-                //   this.result.items = [1, 2];
-                    console.log('--this.result--->');
-                   // console.log(response.body.results);
-                }})(me)
-            
-            , response => {
-                this.spinnerTrigger = false;    
-                console.log('--error---');
-            });
-            */
         }
     }
 }
