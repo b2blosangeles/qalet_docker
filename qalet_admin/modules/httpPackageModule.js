1655a7ab50840c9ece9566c4a817233471150415
b2blosangeles <b2blosangeles@gmail.com>
2020-04-23 22:14:27 -0700

    Update httpPackageModule.js

--- qalet_admin/modules/httpPackageModule.js
+++ qalet_admin/modules/httpPackageModule.js
@@ -21,6 +21,27 @@
 			      }
 			});
 		}
+		this.callA = function(p) {
+			var me = this;
+			var fn = env.adminFolder + '/httpPackage/' + p.replace(/^\//, '') + '.json';
+			pkg.fs.stat(fn, function(err, stat) {
+			      if(err == null) {
+				  if (stat.isDirectory()) {
+					res.render('html/page404.ect');
+				  } else {
+					let cfg = {};
+					try {
+						delete require.cache[fn];
+						cfg = require(fn);
+					}  catch (err) {}
+
+					me.plusFiles(cfg);
+				  }
+			      } else if(err.code === 'ENOENT') {
+				  res.render('html/page404.ect');
+			      }
+			});
+		}
 		this.plusFiles = function(cfg) {
 			
 			var me = this;
