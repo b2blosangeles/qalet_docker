cbfb57fee4ed109e5180d2fd16bf59497156e3ed
b2blosangeles <b2blosangeles@gmail.com>
2020-04-24 00:34:09 -0700

    Update test.js

--- qalet_admin/httpdocs/js/test.js
+++ qalet_admin/httpdocs/js/test.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
                     function() {
                               var todoItem  = Vue.component('todoItem', {
                                         props: ['todo'],
-                                        template: '<h3>-8-{{ todo }}-9- {{message}} -</h3>',
+                                        template: '<h3>-8-{{ todo }}-9- <message title="KAI"></message> -</h3>',
                                         components : {
                                               message   : QALETCOMMA.message
                                          }
