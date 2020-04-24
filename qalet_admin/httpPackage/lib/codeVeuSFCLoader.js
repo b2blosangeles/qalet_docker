dd24d8cdc80b33f1980996ac5737b47e92af18f8
b2blosangeles <b2blosangeles@gmail.com>
2020-04-23 19:22:36 -0700

    Update codeVeuSFCLoader.js

--- qalet_admin/httpPackage/lib/codeVeuSFCLoader.js
+++ qalet_admin/httpPackage/lib/codeVeuSFCLoader.js
@@ -477,6 +477,7 @@
 	codeVeuSFCLoader.scriptExportsHandler = identity;
 
 	function codeVeuSFCLoader(url, name) {
+		var comp = parseComponentURL(url);
 		return codeVeuSFCLoader.load(url, name);
 	}
 
