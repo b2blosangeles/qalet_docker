e4432bbf961adc1ca32948e417762adcf3348073
b2blosangeles <b2blosangeles@gmail.com>
2020-04-24 08:44:22 -0700

    Update httpPackageModule.js

--- qalet_admin/modules/httpPackageModule.js
+++ qalet_admin/modules/httpPackageModule.js
@@ -70,7 +70,7 @@
 							var style = data.match(/\<style\>(.*?)\<\/style\>/im);
 							cbk ({
 								template : encodeURIComponent(template[0]),
-								script : script,
+								script : (!script) ? null : script,
 								style : (!style) ? null : style[1]
 							});
 						}); 
