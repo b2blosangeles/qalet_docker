33d0e083d9a8a3aa8936034b638bdb892ec5f013
b2blosangeles <b2blosangeles@gmail.com>
2020-04-24 15:14:17 -0700

    Update httpPackageModule.js

--- qalet_admin/modules/httpPackageModule.js
+++ qalet_admin/modules/httpPackageModule.js
@@ -101,11 +101,13 @@
 						tmp += 'template : decodeURIComponent("' + CP.data['_' + i].template + '"), '; 
 						tmp += CP.data['_' + i].script + '}); ';
 						tmp = encodeURIComponent(tmp);
-						var v = new Function(decodeURIComponent(tmp));
+						var vv = new Function(decodeURIComponent(tmp));
+						str += vv();
+						/*
 						str += 'try { ' + nameSpace + '.' + fileName;
 						str +=	' = new Function(decodeURIComponent("'+ tmp + '"))() '; 
 						str += '} catch (e) { console.log("' + list[i] + '::" + e.toString()); }' + "\n";
-												
+						*/						
 						css_str += CP.data['_' + i].style;
 						
 					}
