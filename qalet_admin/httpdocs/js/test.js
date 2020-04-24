67a43e27c18bf05e68194490b21fd72bb5f7a495
b2blosangeles <b2blosangeles@gmail.com>
2020-04-23 21:44:27 -0700

    Update test.js

--- qalet_admin/httpdocs/js/test.js
+++ qalet_admin/httpdocs/js/test.js
@@ -1,31 +1,7 @@
-/*        var todoItem  = Vue.component('todo-item', {
-          props: ['todo'],
-          template: '<h2>{{ todo }}</h2>'
-        });
-        */
 $(document).ready(function(){
-          /*
-        new Vue({
-          el: '#jxutest',
-          data: {
-            message : 'niu'
-          },
-          components : {
-          //      todoItem :todoItem
-          },
-          computed: {
-
-          }
-        });
-          var app2 = new Vue({
-            el: '#jxutest',
-            data: {
-              message: 'Hello Vue!'
-            }
-          })*/
           var todoItem  = Vue.component('todo-item', {
                     props: ['todo'],
-                    template: '<h2>{{ todo }}</h2>'
+                    template: '<h3>{{ todo }}</h3>'
           });
           setTimeout(
                     function() {
