44d98fb2844ebe66b2d206c02eb009f73e50d0a3
b2blosangeles <b2blosangeles@gmail.com>
2020-04-25 11:18:41 -0700

    Update spinner.vue

--- qalet_admin/httpPackage/commonModule/spinner.vue
+++ qalet_admin/httpPackage/commonModule/spinner.vue
@@ -107,25 +107,4 @@ module.exports = {
     height:100%;
 }
 
-/*
-.overlay_popup_page {
-    background: transparent; position: fixed;
-    height: 100%; width: 100%;
-    z-index: 5001;
-    top: 0; left: 0;
-    float: left;
-    text-align: center;
-    padding-top: 6em;
-    opacity: 1
-}
-.overlay_popup_cover {
-    position:fixed;
-    top:0;
-    left:0;
-    background:rgba(200,200,200,0.8);
-    z-index:5000;
-    width:100%;
-    height:100%;
-}
-*/
 </style>
