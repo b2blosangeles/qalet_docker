d7e035593c3d00e69740288fdeb665d256ab9b9f
b2blosangeles <b2blosangeles@gmail.com>
2020-04-24 17:26:48 -0700

    Update test.js

--- qalet_admin/httpdocs/js/test.js
+++ qalet_admin/httpdocs/js/test.js
@@ -3,10 +3,7 @@ $(document).ready(function(){
                     function() {
                               var todoItem  = Vue.component('todoItem', {
                                         props: ['todo'],
-                                        template: '<h3>-8-{{ todo }}-9- <message title="KAI"></message> -</h3>',
-                                        components : {
-                                              message   : QALETCOMM.message
-                                         }
+                                        template: '<h3>-8-{{ todo }}-9- <message title="KAI"></message> -</h3>'
                                 });        
                               var app2 = new Vue({
                                 el: '#jxutest',
