8028ab91180c8f21115454a2aa55106aa2aafbe4
b2blosangeles <b2blosangeles@gmail.com>
2020-04-26 05:13:30 -0700

    Update message.vue

--- qalet_admin/httpPackage/appAdmin/message.vue
+++ qalet_admin/httpPackage/appAdmin/message.vue
@@ -1,9 +1,9 @@
 <template>
-    <span>
+   
         hello friend {{title}}
         <pop-up v-if="popUpOn"></pop-up>
         <button type="button" class="btn btn-success"  v-on:click="setPopUp()">Pop Up</button>
-    </span>
+  
 </template>
  
 <script>
