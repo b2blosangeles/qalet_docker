f61820a203a5d5818153b51ff9e03aef70978248
b2blosangeles <b2blosangeles@gmail.com>
2020-04-26 11:26:37 -0700

    Create inputFormA.vue

---
+++ qalet_admin/httpPackage/appAdmin/inputFormA.vue
@@ -0,0 +1,31 @@
+<template>
+      <div class="card" style="width: 18rem;">
+        <div class="card-body">
+          <h5 class="card-title">Input Form keep-alive v9</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <button class="btn btn-primary" v-on:click="closePopup()">clost popup</button>
+        </div>
+      </div>
+</template>
+
+<script>
+module.exports = {
+    props: [],
+   data: function() {  
+        return {
+        }
+    },
+    components : {
+    },
+    methods : {
+
+        closePopup() {
+            this.$parent.closePopup();
+        }
+     
+    }
+}
+</script>
+
+<style>
+</style>
