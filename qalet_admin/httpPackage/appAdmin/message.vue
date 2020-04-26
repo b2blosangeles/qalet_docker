8633e61efad7db0f0e8a0b79dd20f465d2ac49e4
b2blosangeles <b2blosangeles@gmail.com>
2020-04-26 05:06:49 -0700

    Update message.vue

--- qalet_admin/httpPackage/appAdmin/message.vue
+++ qalet_admin/httpPackage/appAdmin/message.vue
@@ -1,13 +1,24 @@
 <template>
     <span>
         hello friend {{title}}
-        <pop-up></pop-up>
+        <pop-up v-if="popUpOn"></pop-up>
+        <button type="button" class="btn btn-warning"  v-on:click="popUp()"><i class="icon-plus-sign-alt"></i> Cancel</button>
     </span>
 </template>
  
 <script>
 module.exports = {
-    props: ['title']
+    props: ['title'],
+    data: function() {  
+        return { 
+            popUpOn : false
+        }
+    },
+    method : {
+        popUp() {
+            popUpOn = true
+        }
+    }
 }
 </script>
 <style>
