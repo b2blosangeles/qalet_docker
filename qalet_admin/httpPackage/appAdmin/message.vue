dc68204b0cee73d2dd68be139b6ced2349f0f20a
b2blosangeles <b2blosangeles@gmail.com>
2020-04-26 10:31:13 -0700

    Update message.vue

--- qalet_admin/httpPackage/appAdmin/message.vue
+++ qalet_admin/httpPackage/appAdmin/message.vue
@@ -1,7 +1,7 @@
 <template>
    <span>
         hello friend {{title}}
-        <pop-up ref="popUp1" v-bind:config="config"></pop-up>
+        <pop-up ref="popUp1" v-bind:dynamicPopup="dynamicPopup"></pop-up>
         <button type="button" class="btn btn-success"  v-on:click="activePopUp()">Pop Up</button>
    </span>
 </template>
@@ -10,7 +10,8 @@
 module.exports = {
     props: ['title'],
     data: function() {  
-        return { 
+        return {
+           dynamicPopup : appAdmin.inputForm,
            config : {
                dynamicPopup : appAdmin.inputForm
            }
