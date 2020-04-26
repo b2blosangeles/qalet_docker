2685e60ac605cc0899630e1c39e3d80da3068d91
b2blosangeles <b2blosangeles@gmail.com>
2020-04-26 10:49:36 -0700

    Update inputForm.vue

--- qalet_admin/httpPackage/appAdmin/inputForm.vue
+++ qalet_admin/httpPackage/appAdmin/inputForm.vue
@@ -1,7 +1,7 @@
 <template>
       <div class="card" style="width: 18rem;">
         <div class="card-body">
-          <h5 class="card-title">Input Form keep-alive v8</h5>
+          <h5 class="card-title">Input Form keep-alive v9</h5>
           <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
           <button class="btn btn-primary" v-on:click="closePopup()">clost popup</button>
         </div>
