434566e45783a11ef31892c317900d01737bc3fa
b2blosangeles <b2blosangeles@gmail.com>
2020-04-25 21:49:47 -0700

    Update main.js

--- qalet_admin/httpPackage/appAdmin/main.js
+++ qalet_admin/httpPackage/appAdmin/main.js
@@ -1,3 +1,23 @@
+if (!_qalet) var _qalet = {};
+
+(function ($) {
+    $.fn.serializeFormJSON = function () {
+        var o = {};
+        var a = this.serializeArray();
+        $.each(a, function () {
+            if (o[this.name]) {
+                if (!o[this.name].push) {
+                    o[this.name] = [o[this.name]];
+                }
+                o[this.name].push(this.value || '');
+            } else {
+                o[this.name] = this.value || '';
+            }
+        });
+        return o;
+    };
+})(jQuery);
+
 $(document).ready(function(){
         var globalComponents = {
               dbList         : appAdmin.dbList,
