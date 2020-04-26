579b7b31a23ea8c864acc99124c8fed5ca8f56db
b2blosangeles <b2blosangeles@gmail.com>
2020-04-25 18:21:17 -0700

    Create main.js

---
+++ qalet_admin/httpPackage/appAdmin/main.js
@@ -0,0 +1,50 @@
+$(document).ready(function(){
+        var globalComponents = {
+              dbList         : appAdmin.dbList,
+              hostList       : appAdmin.hostList
+        }
+        var NotFound = { 
+            template: '<message postTitle="Page not found"></message>',
+            components : globalComponents 
+         },
+         Home = { 
+            template: '<message postTitle="Home"></message>',
+            components : globalComponents 
+         },
+         About = { 
+            template: '<message postTitle="About"> </message>',
+            components : globalComponents 
+         },
+         Databases = { 
+            template: '<db-list postTitle="Databases"></db-list>',
+            components : globalComponents 
+         },
+         virtualHosts = { 
+            template: '<host-list postTitle="Databases"></host-list>',
+            components : globalComponents 
+         };
+    
+        var routes = {
+          '/'               : Home,
+          '/about'          : About,
+          '/dbs'            : Databases,
+          '/virtualHosts'   : virtualHosts
+        }
+    
+        new Vue({
+          el: '#app',
+          data: {
+            currentRoute: window.location.pathname
+          },
+          computed: {
+            ViewComponent () {
+              return routes[this.currentRoute] || NotFound
+            }
+          },
+          render (h) { return h(this.ViewComponent) }
+        });
+   
+
+    
+});
+© 2020 GitHub, Inc.
