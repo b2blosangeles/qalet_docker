9a3dce6e8f5ead6fe04e4a6a05e6b95d3a316422
b2blosangeles <b2blosangeles@gmail.com>
2020-04-26 05:08:08 -0700

    Update message.vue

--- qalet_admin/httpPackage/appAdmin/message.vue
+++ qalet_admin/httpPackage/appAdmin/message.vue
@@ -2,7 +2,7 @@
     <span>
         hello friend {{title}}
         <pop-up v-if="popUpOn"></pop-up>
-        <button type="button" class="btn btn-warning"  v-on:click="popUp()"><i class="icon-plus-sign-alt"></i> Cancel</button>
+        <button type="button" class="btn btn-warning"  v-on:click="popUp()"><i class="icon-plus-sign-alt"></i> Pop Up</button>
     </span>
 </template>
  
