17ce4e79b91cb362b6574aa4c281778fd22ccf9d
b2blosangeles <b2blosangeles@gmail.com>
2020-04-26 06:04:08 -0700

    Update message.vue

--- qalet_admin/httpPackage/appAdmin/message.vue
+++ qalet_admin/httpPackage/appAdmin/message.vue
@@ -23,7 +23,7 @@ module.exports = {
             this.popUpOn = v
         },
         callPopUp(v) {
-           this.$children.popup.fromParent()
+           this.$children.popUp.fromParent()
         }
     }
 }
