9586fbbb2fc21b3d782f002653d7a03697300aba
b2blosangeles <b2blosangeles@gmail.com>
2020-04-26 05:54:58 -0700

    Update popUp.vue

--- qalet_admin/httpPackage/commonModule/popUp.vue
+++ qalet_admin/httpPackage/commonModule/popUp.vue
@@ -21,6 +21,9 @@ module.exports = {
     methods : {
         closePopup() {
             this.$parent.setPopUp(false);
+        },
+        fromParent() {
+            alert('fromParent');
         }
     }
 }
