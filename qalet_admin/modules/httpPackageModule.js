a2fff7dc291442b6c25864f9ae763d2bf210196f
b2blosangeles <b2blosangeles@gmail.com>
2020-04-25 21:35:56 -0700

    Update httpPackageModule.js

--- qalet_admin/modules/httpPackageModule.js
+++ qalet_admin/modules/httpPackageModule.js
@@ -8,15 +8,16 @@
 			    dirName = env.adminFolder + '/httpPackage/' + appName,
 			    fn = dirName + '.json';
 			
+			res.send(cfg);
+			return true;
+			/*		
 			
 			try {
 				delete require.cache[fn];
 				var cfg = require(fn);
 			}  catch (err) {};
 			
-			res.send(cfg);
-			return true;
-			/*
+
 			_f['common'] = function(cbk) {
 				var dirCommon = env.adminFolder + '/httpPackage/commonModule'; 
 				pkg.fs.readdir(dirCommon, (err, files) => {
