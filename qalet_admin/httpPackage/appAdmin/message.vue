c5cdcb3159851e517b421288692c733098412101
b2blosangeles <b2blosangeles@gmail.com>
2020-04-26 07:44:18 -0700

    Update message.vue

--- qalet_admin/httpPackage/appAdmin/message.vue
+++ qalet_admin/httpPackage/appAdmin/message.vue
@@ -11,7 +11,7 @@ module.exports = {
     props: ['title'],
     data: function() {  
         return { 
-           popupConfig : {inputFrom : appadmin.inputFrom}
+           popupConfig : {inputForm : appadmin.inputForm}
         }
     },
     components : {
