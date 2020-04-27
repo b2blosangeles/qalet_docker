d7a8cacb2f0b482d51215a768d80494f5e2a0b24
b2blosangeles <b2blosangeles@gmail.com>
2020-04-26 17:20:53 -0700

    Create alert.vue

---
+++ qalet_admin/httpPackage/commonModule/alert.vue
@@ -0,0 +1,65 @@
+<template>
+    <div v-if="showPopup">
+        <div class="overlay_popup_cover"></div>
+        <div class="overlay_popup">
+            <div class="popup_body rounded shadow">
+                <button type="button" class="close m-2" aria-label="Close" v-on:click="closePopup()" v-if="!config.noCloseIcon">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+                <keep-alive>
+                  <component :is="config.dynamicPlugin"></component>
+                </keep-alive>          
+            </div>
+        </div>
+    </div>    
+</template>
+
+<script>
+module.exports = {
+   props: ["config"],
+   data: function() {  
+        return {
+            showPopup : false
+        }
+    },
+    methods : {
+        closePopup() {
+            this.showPopup = false;
+        },    
+        activePopUp() {
+            this.showPopup = true;
+        }
+    }
+}
+</script>
+
+<style>
+
+.overlay_popup {
+    background: transparent; position: fixed;
+    height: 100%; width: 100%;
+    z-index: 5001;
+    top: 0; left: 0;
+    text-align: center;
+    padding-top: 6em;
+    opacity: 1
+}
+.overlay_popup_cover {
+    position:fixed;
+    top:0;
+    left:0;
+    background:rgba(200,200,200,0.8);
+    z-index:5000;
+    width:100%;
+    height:100%;
+}
+
+ 
+.popup_body {
+    background-color: #ffff;
+    width : 80%;
+    min-height : 6em;
+    margin: 0 auto;
+}
+
+</style>
