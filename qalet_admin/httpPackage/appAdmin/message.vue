daa28e93d331e0d3f4ef39ea756031255ff98ef6
b2blosangeles <b2blosangeles@gmail.com>
2020-04-26 17:28:29 -0700

    Update message.vue

--- qalet_admin/httpPackage/appAdmin/message.vue
+++ qalet_admin/httpPackage/appAdmin/message.vue
@@ -2,6 +2,7 @@
    <span>
         hello friend {{title}}  
         <pop-up ref="popUp1" v-bind:config="popUpConfig()"></pop-up>
+        <alert ref="alert1" v-bind:config="popUpConfig()"></alert>
         <button type="button" class="btn btn-success m-2"  v-on:click="activePopUp('')">Form</button>
         <button type="button" class="btn btn-warning m-2"  v-on:click="activePopUp('A')">Form A</button>
         <button type="button" class="btn btn-success m-2"  v-on:click="activePopUp('A')">Alert</button>
