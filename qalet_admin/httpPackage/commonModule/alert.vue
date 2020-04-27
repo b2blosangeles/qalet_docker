1d1de4c5aa7f8d062601179229ebc17f33b939f5
b2blosangeles <b2blosangeles@gmail.com>
2020-04-26 18:56:18 -0700

    Update alert.vue

--- qalet_admin/httpPackage/commonModule/alert.vue
+++ qalet_admin/httpPackage/commonModule/alert.vue
@@ -3,7 +3,7 @@
         <div class="overlay_alert_cover"></div>
         <div class="overlay_alert">
             <div class="alert_body">
-                <div class="alert alert-primary" role="alert">
+                <div class="alert alert-success" role="alert">
                   This is a primary alert—check it out!
                 </div>
             </div>
